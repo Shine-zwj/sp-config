1b402e877f0706b2d63f4159f8b69963011a84e5
ShineKOT <1917095344@qq.com>
2020-03-07 16:12:42 +0800

    1

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,5 +1,35 @@
 package test;
 
 public class Test {
-
+	public static void main(String[] args) {
+		int n = 127;
+		Integer n0 = 127;
+		Integer n1 = 127;
+		Integer n2 = Integer.valueOf(127);
+		System.out.println(n0==n1);
+		System.out.println(n1==n2);
+		System.out.println(n0==n2);
+		System.out.println(n==n0);
+		Integer n3 = new Integer(127);
+		Integer n4 = new Integer(127);
+		System.out.println(n==n3);
+		System.out.println(n0==n3);
+		System.out.println(n3==n4);
+		
+		System.out.println("==========================");
+		
+		int m = 128;
+		Integer m0 = 128;
+		Integer m1 = 128;
+		Integer m2 = Integer.valueOf(128);
+		System.out.println(m0==m1);
+		System.out.println(m1==m2);
+		System.out.println(m0==m2);
+		System.out.println(m==m0);
+		Integer m3 = new Integer(128);
+		Integer m4 = new Integer(128);
+		System.out.println(m==m3);
+		System.out.println(m0==m3);
+		System.out.println(m3==m4);
+	}
 }
